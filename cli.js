61bd14ed98abbae5e16622a223919cec5988e04d
Williams Omayuku <womayuku@gmail.com>
2016-12-12 10:04:18 +0100

    Fixed default value for "view". Will now default to root if no argument is passed

--- cli.js
+++ cli.js
@@ -3,7 +3,7 @@
 
 
 var program = require('commander'), fs = require('fs'), nconf = require('nconf'), fileview = require('./index');
-var pkg = JSON.parse(fs.readFileSync('${__dirname}/../package.json'));
+var pkg = JSON.parse(fs.readFileSync(__dirname + '/package.json'));
 
 program
 	.version(pkg.version)
@@ -11,7 +11,8 @@ program
 	.option('-t, --token <value>', 'Enter your dropbox authentication token')
 	.option('-d, --download [value]', 'Enter the full dropbox path of the file to be downloaded (Downloads to working directory')
 	.option('-u, --upload [value]', 'Enter the full relative path of the file to be uploaded (Uploads to the dropbox root')
-	.option('-v, --view [value]', 'Enter the path of the dropbox folder to view or enter "#root" to view the root folder')
+	.option('-v, --view [value]', 'Enter the path of the dropbox folder to view or enter "#root" to view the root folder', '#root')
+	.option('-d, --disable', 'Use this flag to revoke the access token')
 	.parse(process.argv);
 
 	
@@ -19,6 +20,7 @@ program.on('--help', ()=>{
 	console.log('  Fileview fetches authentication token from either of 2 means:');
 	console.log('    **Config file at /config.json [RECOMMENDED]');
 	console.log('    **Commandline option; "-t" or "--token" flag [OVERRIDES THE CONFIG');
+	console.log(__dirname);
 	
 });
 
@@ -37,5 +39,7 @@ if(!program.token && !nconf.get('DROPBOX_TOKEN')) {
 else if(!program.token) {
 	program.token = nconf.get('DROPBOX_TOKEN');
 }
+
+
 
 fileview(program);
