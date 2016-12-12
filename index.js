861803f98af9e4107bba623fecec86aaabe8aee3
Williams Omayuku <womayuku@gmail.com>
2016-12-12 10:09:34 +0100

    Fixed argument parsing. Added option to revoke token authorization

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const Dropbox = require('dropbox'), fs = require('fs'), path = require('path');
-//let dbx = new Dropbox({accessToken: });
 
 function fileview(arg){
-	if(arg.upload) {upload(arg.upload)}
-	else if (arg.view) {show(arg.view)}
-	else if (arg.download) {download(arg.download)}
+	if(arg.upload) {upload(arg)}
+	else if (arg.view) {view(arg)}
+	else if (arg.download) {download(arg)}
+	else if (arg.disable) {disable(arg)}
 	else {
-		console.log('Error: unknown arguments please run --help || -h');
+		console.log('Error: unknown arguments. Please run --help || -h');
 		process.exit(1);
 	}
 }
@@ -19,11 +19,9 @@ var upload = (arg)=>{
 	var dbx = new Dropbox({accessToken: arg.token});
 	return fs.readFile(arg.upload, (err, file)=>{
 		if(err) throw err;
-		//console.log(JSON.stringify(file));
 		dbx.filesUpload({
 			'contents': file,
 			'path': '/' + path.basename(arg.upload),
-			//'mode': {'.tag': 'update'},
 			'autorename': true,
 			'mute': true
 		}).then(res=>{console.log("Done uploading: " + JSON.stringify(res, null, 2))})
@@ -32,7 +30,7 @@ var upload = (arg)=>{
 };
 
 //Function to view files in Dropbox
-var show = (arg)=>{
+var view = (arg)=>{
 	var dbx = new Dropbox({accessToken: arg.token});
 	if (arg.view === '#root') {arg.view = ''};
 	return dbx.filesListFolder({path: arg.view})
@@ -58,5 +56,13 @@ var download = (arg)=>{
 		console.log("Done downloading: " + JSON.stringify(res, null, 2))})
 	.catch(err=>{console.error("Something went wrong: %s", JSON.stringify(err, null, 2))});
 };
+
+//Function to diable access token
+var disable = (arg)=>{
+	var dbx = new Dropbox({accessToken: arg.token});
+	return dbx.authTokenRevoke()
+	.then(()=>{console.log('Access token successfully revoked')})
+	.catch(err=>{console.log('Something went wrong: ' + err)});
+}
 
 module.exports = fileview;
